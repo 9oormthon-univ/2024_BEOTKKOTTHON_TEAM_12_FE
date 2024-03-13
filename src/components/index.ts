cfeeff22621da17d2545bed9839b54ce64d3b816
102sae <102chimee@gmail.com>
2024-03-14 01:35:05 +0900

    FEAT : 프로그레스바 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as TextLabel } from './atom/text-label';
 export { default as ProfileButton } from './atom/profile-button';
 //Molcule
 export { default as ProfileCard } from './molcule/profile-card';
+export { default as EcoProgressBar } from './molcule/eco-progressbar';
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { ProfileCard } from '../../components/index';
+import ProgressBar from '../../components/molcule/eco-progressbar';
 
 const MyPage: React.FC = () => {
+  const currentLevel = {
+    level: '새싹',
+    nextLevel: '목화',
+    currentExp: 100,
+    nextLevelExp: 200,
+  };
   return (
     <div>
       <ProfileCard />
+      <ProgressBar
+        level={currentLevel.level}
+        nextLevel={currentLevel.nextLevel}
+        currentExp={currentLevel.currentExp}
+        nextLevelExp={currentLevel.nextLevelExp}
+      />
     </div>
   );
 };
