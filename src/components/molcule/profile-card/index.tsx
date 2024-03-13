68075f36a286f5dc999f7d2ddaaddb9f4b8e319e
102sae <102chimee@gmail.com>
2024-03-14 01:37:13 +0900

    STYLE : import 변경

--- src/components/molcule/profile-card/index.tsx
+++ src/components/molcule/profile-card/index.tsx
@@ -1,56 +1,9 @@
 import React from 'react';
+import * as S from './style';
 import { ProfileAvatar, ProfileButton, TextLabel } from '../../index';
 import defaultImg from '../../../assets/images/profile-default-image.svg';
 import arrow from '../../../assets/icons/arrow.svg';
 import profileIconLevel1 from '../../../assets/icons/profile-icon-level1.svg';
-import styled from 'styled-components';
-
-export const ProfileCardWrapper = styled.div`
-  width: 80%;
-  display: flex;
-  justify-content: space-around;
-  padding: 10px 15px;
-  margin: 20px auto 10px auto;
-  border-radius: 10px;
-
-  background-color: var(--grey-1);
-  border: 1px solid var(--grey-3);
-`;
-
-export const RightContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const MiddleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 100%;
-  padding: 10px;
-  gap: 4px;
-`;
-
-export const LeftContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  cursor: pointer;
-`;
-
-export const UserNameWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-`;
-
-export const ButtonContainer = styled.div`
-  display: flex;
-  margin-top: 3px;
-  gap: 3px;
-`;
-
-export const Image = styled.img``;
 
 const ProfileCard: React.FC = () => {
   const userProfile = {
@@ -66,19 +19,19 @@ const ProfileCard: React.FC = () => {
   };
 
   return (
-    <ProfileCardWrapper>
-      <RightContainer>
+    <S.ProfileCardWrapper>
+      <S.RightContainer>
         <ProfileAvatar imageUrl={defaultImg} />
-      </RightContainer>
+      </S.RightContainer>
 
-      <MiddleContainer>
-        <UserNameWrapper>
+      <S.MiddleContainer>
+        <S.UserNameWrapper>
           <TextLabel text={userProfile.nickname} size={18} />
-          <Image src={profileIconLevel1} alt="profile level" />
-        </UserNameWrapper>
+          <S.Image src={profileIconLevel1} alt="profile level" />
+        </S.UserNameWrapper>
         <TextLabel text={userProfile.university} size={16} color="var(--grey-5)" />
 
-        <ButtonContainer>
+        <S.ButtonContainer>
           {userProfile.style.map((style, index) => (
             <ProfileButton
               key={index}
@@ -87,13 +40,13 @@ const ProfileCard: React.FC = () => {
               borderColor="var(--grey-3)"
             />
           ))}
-        </ButtonContainer>
-      </MiddleContainer>
+        </S.ButtonContainer>
+      </S.MiddleContainer>
 
-      <LeftContainer>
-        <Image src={arrow} alt="프로필 수정" onClick={onModifyProfile} />
-      </LeftContainer>
-    </ProfileCardWrapper>
+      <S.LeftContainer>
+        <S.Image src={arrow} alt="프로필 수정" onClick={onModifyProfile} />
+      </S.LeftContainer>
+    </S.ProfileCardWrapper>
   );
 };
 
