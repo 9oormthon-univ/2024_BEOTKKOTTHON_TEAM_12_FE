989065f295ccee1075ed01d4f746f8d410edf0ef
김동균 <wooheuy5752@nate.com>
2024-03-14 21:08:58 +0900

    FEAT : SearchPage 포커스 이벤트 구현

--- src/components/atom/search/index.tsx
+++ src/components/atom/search/index.tsx
@@ -2,8 +2,18 @@ import { useState } from 'react';
 import * as S from './style';
 import search from '@assets/icons/search.svg';
 
-const Search = () => {
+interface SearchProps {
+  setFocus: (value: boolean) => void;
+}
+
+const Search = ({ setFocus }: SearchProps) => {
   const [searchInput, SetSearchInput] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    SetSearchInput(value);
+  };
+
   return (
     <S.Container>
       <div>
@@ -11,7 +21,9 @@ const Search = () => {
       </div>
       <input
         value={searchInput}
-        onChange={(e) => SetSearchInput(e.target.value)}
+        onChange={handleChange}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
         placeholder="무엇이든 검색해보세요."
       />
     </S.Container>
--- src/components/molcule/header-search/index.tsx
+++ src/components/molcule/header-search/index.tsx
@@ -1,10 +1,14 @@
 import { Search } from '@components/index';
 import * as S from './style';
 
-const HeaderSearch = () => {
+interface HeaderSearchProps {
+  setFocus: (value: boolean) => void;
+}
+
+const HeaderSearch = ({ setFocus }: HeaderSearchProps) => {
   return (
     <S.Container>
-      <Search />
+      <Search setFocus={setFocus} />
       <p className="cancle">취소</p>
     </S.Container>
   );
