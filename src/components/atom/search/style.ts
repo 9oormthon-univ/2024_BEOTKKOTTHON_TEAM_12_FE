99232426e66e726508981a02918bf33de3a65486
김동균 <wooheuy5752@nate.com>
2024-03-19 14:47:53 +0900

    FIX : 검색창 기본 X 표시 스타일링 제거 및 배경 색상 지정

--- src/components/atom/search/style.ts
+++ src/components/atom/search/style.ts
@@ -21,9 +21,23 @@ export const Container = styled.div`
     height: 24px;
     border: none;
     outline: none;
+    background-color: white;
 
     &::placeholder {
       color: var(--grey-4);
+    }
+
+    &::-ms-clear,
+    &::-ms-reveal {
+      display: none;
+      width: 0;
+      height: 0;
+    }
+    &::-webkit-search-decoration,
+    &::-webkit-search-cancel-button,
+    &::-webkit-search-results-button,
+    &::-webkit-search-results-decoration {
+      display: none;
     }
   }
 `;
