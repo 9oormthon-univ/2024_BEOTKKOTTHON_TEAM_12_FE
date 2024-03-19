ff4d1356533bbfafd3c06e74ea50f3835bff5140
김동균 <wooheuy5752@nate.com>
2024-03-19 10:43:14 +0900

    FEAT : 스플래쉬 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import { Router } from '@components/index';
 import GlobalStyle from './styles/GlobalStyle';
+import { useEffect, useState } from 'react';
+import splash from '@assets/logo/splash.svg';
 
 function App() {
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="root">
       <GlobalStyle />
-      <Router />
+      {showSplash ? (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <img src={splash} alt="splash" />
+        </div>
+      ) : (
+        <Router />
+      )}
     </div>
   );
 }
