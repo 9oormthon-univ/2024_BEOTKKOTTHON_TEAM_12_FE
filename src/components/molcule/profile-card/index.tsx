50caea867d059dc22ee546b9754a1adce345ea58
김동균 <wooheuy5752@nate.com>
2024-03-22 13:56:08 +0900

    FEAT : 마이페이지 서버 연결

--- src/components/molcule/profile-card/index.tsx
+++ src/components/molcule/profile-card/index.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './style';
 import { ProfileAvatar, ProfileButton, TextLabel } from '../../index';
 import defaultImg from '@assets/images/profile-default-image.svg';
 import arrow from '@assets/icons/arrow.svg';
 import { levelUrlArr } from 'src/utils/levelUrlArr';
 import { useNavigate } from 'react-router-dom';
-import useStore from '../../../store/userData';
+import useStore, { useUserProfileInfo } from '../../../store/userData';
+import { instance } from 'src/apis';
 
 const ProfileCard: React.FC = () => {
-  const { userProfileInfo } = useStore();
+  const { updateUserProfileInfo } = useStore();
   const navigate = useNavigate();
-  const userProfile = userProfileInfo;
+  const userProfile = useUserProfileInfo();
+
+  // 로그인된 사용자 id 필요
+  const userId = 1;
+
+  const getData = async () => {
+    await instance
+      .get(`users/${userId}`)
+      .then((res) => {
+        console.log('마이페이지 데이터 가져오기 성공', res);
+        updateUserProfileInfo(res.data);
+      })
+      .catch((e) => {
+        console.log('마이페이지 데이터 가져오기 실패', e);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onModifyProfile = () => {
     navigate('/mypage/profile-edit');
@@ -25,7 +45,7 @@ const ProfileCard: React.FC = () => {
       <S.MiddleContainer>
         <S.UserNameWrapper>
           <TextLabel text={userProfile.user_name} size={18} />
-          <S.Image src={levelUrlArr(2)} alt="profile level" />
+          <S.Image src={levelUrlArr(userProfile.level)} alt="profile level" />
         </S.UserNameWrapper>
         <TextLabel text={userProfile.university_name} size={16} color="var(--grey-5)" />
 
