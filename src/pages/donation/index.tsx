74ad2c2372304855c39cd49871e2ab464e9146d7
김동균 <wooheuy5752@nate.com>
2024-03-22 13:09:49 +0900

    STYLE : 기부 단체 박스 스타일 변경

--- src/pages/donation/index.tsx
+++ src/pages/donation/index.tsx
@@ -38,7 +38,9 @@ const Donation = () => {
             />
           </S.MapWrapper>
 
-          <BoxDonationImg />
+          <div className="partner-box">
+            <BoxDonationImg />
+          </div>
         </section>
       </S.Content>
 
--- src/pages/donation/style.ts
+++ src/pages/donation/style.ts
@@ -14,6 +14,13 @@ export const Content = styled.div`
   .ranking {
     margin-bottom: 14px;
   }
+
+  .partner-box {
+    width: 335px;
+    padding: 0px 33px 20px 33px;
+
+    background-color: white;
+  }
 `;
 export const MapWrapper = styled.div`
   display: flex;
