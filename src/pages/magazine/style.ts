17cc07f741768bfacbdedbd7434fdf4437e01e02
김동균 <wooheuy5752@nate.com>
2024-03-23 22:50:12 +0900

    FIX: 매거진 헤더 제거

--- src/pages/magazine/style.ts
+++ src/pages/magazine/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  height: var(--content-size);
+  height: calc(var(--content-size) + var(--header-size));
   padding: 0 20px;
   overflow-y: scroll;
   -ms-overflow-style: none;
