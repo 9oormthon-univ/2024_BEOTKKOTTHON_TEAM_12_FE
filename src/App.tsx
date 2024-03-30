baf0bea4c222557b9aa11e8e1edcd0f03e79aa80
김동균 <wooheuy5752@nate.com>
2024-03-30 15:19:58 +0900

    FEAT : 리액트 쿼리 도입

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Router } from 'components/index';
 import GlobalStyle from './styles/GlobalStyle';
 import { useEffect, useState } from 'react';
 import splash from 'assets/logo/splash.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   const [showSplash, setShowSplash] = useState<boolean>(true);
+  const queryClient = new QueryClient();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,27 +19,30 @@ function App() {
 
   return (
     <div className="root">
-      <GlobalStyle />
-      {showSplash ? (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            background: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 1000,
-          }}
-        >
-          <img src={splash} alt="splash" />
-        </div>
-      ) : (
-        <Router />
-      )}
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        {showSplash ? (
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              background: 'white',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              zIndex: 1000,
+            }}
+          >
+            <img src={splash} alt="splash" />
+          </div>
+        ) : (
+          <Router />
+        )}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
   );
 }
