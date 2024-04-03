f91d06fdc59fd06e4cc56b8857eb7a2611085540
김동균 <wooheuy5752@nate.com>
2024-04-03 15:01:05 +0900

    REFACTOR : 이미지 업로드 리액트 쿼리 적용

--- src/components/atom/box-upload/index.tsx
+++ src/components/atom/box-upload/index.tsx
@@ -1,33 +1,40 @@
 import * as S from './style';
 import image from 'assets/icons/image.svg';
 import { useFormDataActions, useShowImages } from 'store/formData';
-import axios from 'axios';
+import { instance } from 'apis';
+import { useMutation } from '@tanstack/react-query';
+
+const postImgData = async (sendImgData: FormData) => {
+  const response = await instance.post('http://43.201.189.171:8080/api/upload', sendImgData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  console.log('상품 이미지 업로드 성공', response);
+  return response.data;
+};
 
 const BoxUpload = () => {
   const showImages = useShowImages();
   const { setFormData, setShowImages } = useFormDataActions();
 
+  const ImgUploadMutation = useMutation({
+    mutationFn: (sendData: FormData) => postImgData(sendData),
+    onSuccess: (data) => setFormData('product_image', data),
+    onError: (error) => console.log('상품 이미지 업로드 실패', error),
+  });
+
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files as FileList;
     const sendImgData = new FormData();
 
-    for (let i = 0; i < files.length; i++) {
-      sendImgData.append('files', files[i]);
-    }
-
-    await axios
-      .post('http://43.201.189.171:8080/api/upload', sendImgData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then((res) => {
-        console.log('상품 이미지 업로드 성공', res);
-        setFormData('product_image', res.data);
-      })
-      .catch((e) => console.log('상품 이미지 업로드 실패', e));
-
     if (files) {
+      for (let i = 0; i < files.length; i++) {
+        sendImgData.append('files', files[i]);
+      }
+
+      ImgUploadMutation.mutate(sendImgData);
+
       const imageLists = files;
       let imageUrlLists = [...showImages];
 
