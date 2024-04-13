8d279861765ea5377f000782a1798fd13cf50011
김동균 <wooheuy5752@nate.com>
2024-04-13 15:50:33 +0900

    FEAT : 상품 수정 페이지 쿼리 데이터 가져오기

--- src/pages/productPage/product-edit/index.tsx
+++ src/pages/productPage/product-edit/index.tsx
@@ -1,26 +1,17 @@
-import { FormTrade, Header } from 'components/index';
+import { FormTrade, Header, Loading } from 'components/index';
 import * as S from './styled';
 import close from 'assets/icons/close_large.svg';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useFormData, useFormDataActions } from 'store/formData';
-import { useEffect } from 'react';
-import { useProductDetailQuery } from 'hooks/queries/products/useProductDetailQuery';
+import { useFormData } from 'store/formData';
 import { useEditProductMutation } from 'hooks/queries/products/useEditProductMutation';
+import { useProductEditQuery } from 'hooks/queries/products/useProductEditQuery';
 
 const ProductEdit = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const formData = useFormData();
-  const { receiveData } = useFormDataActions();
-  const { data: productDetailQuery } = useProductDetailQuery(id as string);
+  const { data: productEditQuery, isLoading, isError } = useProductEditQuery(id as string);
   const { mutate: editProductMutation } = useEditProductMutation(id as string);
-
-  useEffect(() => {
-    if (productDetailQuery) {
-      receiveData(productDetailQuery);
-      console.log('formData', formData);
-    }
-  }, [productDetailQuery]);
 
   return (
     <>
@@ -29,9 +20,16 @@ const ProductEdit = () => {
         <img src={close} className="right" alt="btn-close" onClick={() => navigate(-1)} />
       </Header>
 
-      <S.Content>
-        <FormTrade handleSubmitAction={() => editProductMutation(formData)} btnText={'수정 완료'} />
-      </S.Content>
+      {isLoading ? (
+        <Loading $height="calc(100svh - var(--header-size))" />
+      ) : (
+        <S.Content>
+          <FormTrade
+            handleSubmitAction={() => editProductMutation(formData)}
+            btnText={'수정 완료'}
+          />
+        </S.Content>
+      )}
     </>
   );
 };
--- src/pages/productPage/product-new/index.tsx
+++ src/pages/productPage/product-new/index.tsx
@@ -3,10 +3,17 @@ import * as S from './style';
 import close from 'assets/icons/close_large.svg';
 import { useNavigate } from 'react-router-dom';
 import { useNewProductMutation } from 'hooks/queries/products/useNewProductMutation';
+import { useEffect } from 'react';
+import { useProductListActions } from 'store/productListData';
 
 const ProductNew = () => {
   const navigate = useNavigate();
+  const { setActiveCategory } = useProductListActions();
   const { mutate: newProductMutation } = useNewProductMutation();
+
+  useEffect(() => {
+    setActiveCategory('');
+  }, []);
 
   return (
     <>
