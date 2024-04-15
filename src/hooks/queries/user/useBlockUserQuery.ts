3196f54d1428040c89178141e7331f831159e15e
김동균 <wooheuy5752@nate.com>
2024-04-15 17:22:14 +0900

    FIX : 차단한 사용자 api 수정

--- src/hooks/queries/user/useBlockUserQuery.ts
+++ src/hooks/queries/user/useBlockUserQuery.ts
@@ -5,9 +5,9 @@ import { userId } from 'data/shared';
 const getBlockUserList = async () => {
   console.log('차단한 사용자 목록 불러오기');
   try {
-    const response = await instance.get(`/users/blockedUsers/${userId}pageNumber=0`);
-    console.log('차단한 사용자 목록 불러오기 성공:', response.data);
-    return response.data;
+    const response = await instance.get(`/users/blockedUsers/${userId}?pageNumber=0`);
+    console.log('차단한 사용자 목록 불러오기 성공:', response.data.content);
+    return response.data.content;
   } catch (error: any) {
     console.error('차단한 사용자 목록 불러오기 실패:', error);
     throw new Error(error.response?.data?.message);
