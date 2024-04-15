3dee49a7b71bc1b5bcc5b0d27c5e8ead15cfe169
김동균 <wooheuy5752@nate.com>
2024-04-15 17:04:50 +0900

    FEAT : 차단한 사용자 에러 처리

--- src/components/molcule/list-block-user/index.tsx
+++ src/components/molcule/list-block-user/index.tsx
@@ -1,44 +1,48 @@
 import * as S from './style';
-import { useBlockUserQuery } from 'hooks/queries/user/useBlockUserQuery';
-import { useDeleteBlockUserMutation } from 'hooks/queries/user/useDeleteBlockUserMutation';
 import Loading from 'components/atom/loading';
-import { Button, ProfileAvatar, TextLabel } from 'components';
+import { BoxError, Button, ProfileAvatar, TextLabel } from 'components';
 import { levelUrlArr } from 'utils/levelUrlArr';
 import { BlockedUserType } from 'types/userType';
 import { useUnblockMutation } from 'hooks/queries/user/useUnblockMutation';
 
-const ListBlockUser = () => {
-  const { data: blockedUserList, isLoading, isError } = useBlockUserQuery();
-  const { mutate: unblockMutation } = useUnblockMutation();
+interface ListBlockUser {
+  userList: BlockedUserType[];
+  status: string;
+}
 
-  if (isLoading) return <Loading />;
+const ListBlockUser = ({ userList, status }: ListBlockUser) => {
+  const { mutate: unblockMutation } = useUnblockMutation();
+  const height = `calc(100svh - var(--header-size))`;
+
+  if (status === 'pending') return <Loading $height={height} />;
+  if (status === 'error')
+    return <BoxError $height={height}>차단한 사용자를 불러오지 못했습니다.</BoxError>;
+  if (userList.length === 0) return <BoxError $height={height}>차단한 사용자가 없습니다.</BoxError>;
 
   return (
     <S.Container>
-      {isLoading && <Loading />}
-      {blockedUserList &&
-        blockedUserList.map((blockUser: BlockedUserType, index: number) => (
-          <S.BoxUser key={index}>
-            <ProfileAvatar imageUrl={blockUser.blocked_user_profile_image} />
+      {userList.map((blockUser: BlockedUserType, index: number) => (
+        <S.BoxUser key={index}>
+          <ProfileAvatar imageUrl={blockUser.blocked_user_profile_image} />
 
-            <S.UserName>
-              <TextLabel size={16}>{blockUser.blocked_user_nick_name}</TextLabel>
-              <img src={levelUrlArr(blockUser.blocked_user_level)} width={11} height={11} />
-            </S.UserName>
+          <S.UserName>
+            <TextLabel size={16}>{blockUser.blocked_user_nick_name}</TextLabel>
+            <img src={levelUrlArr(blockUser.blocked_user_level)} width={11} height={11} />
+          </S.UserName>
 
-            <Button
-              width="77px"
-              $padding="5px 10px"
-              $borderRadius="10px"
-              $bgcolor="var(--grey-2)"
-              fontSize="14px"
-              color="var(--grey-5)"
-              handleOnClick={() => unblockMutation(blockUser.blocked_user_id)}
-            >
-              차단해제
-            </Button>
-          </S.BoxUser>
-        ))}
+          <Button
+            width="77px"
+            $padding="5px 10px"
+            $borderRadius="10px"
+            $bgcolor="var(--grey-2)"
+            fontSize="14px"
+            color="var(--grey-5)"
+            handleOnClick={() => unblockMutation(blockUser.blocked_user_id)}
+          >
+            차단해제
+          </Button>
+        </S.BoxUser>
+      ))}
     </S.Container>
   );
 };
