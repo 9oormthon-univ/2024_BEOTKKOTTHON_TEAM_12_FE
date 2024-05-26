0759b7f593ec829a8a96075dd95ef2872198ad43
김동균 <wooheuy5752@nate.com>
2024-05-26 16:50:08 +0900

    Refactor : 파라미터를 mutation 함수 내부로 이동

--- src/components/molcule/footer-product-detail/index.tsx
+++ src/components/molcule/footer-product-detail/index.tsx
@@ -17,7 +17,7 @@ interface FooterProductDetailProps {
 const FooterProductDetail = ({ product, status }: FooterProductDetailProps) => {
   const { mutate: likedMutation } = useLikedMutation(product?.id as number);
   const { mutate: unlikedMutation } = useUnlikedMutation(product?.id as number);
-  const { mutate: roomIdMutation } = useNewChatRoom(product?.id);
+  const { mutate: roomIdMutation } = useNewChatRoom();
   const navigate = useNavigate();
 
   const isMine = product?.seller.id === userId;
@@ -26,7 +26,7 @@ const FooterProductDetail = ({ product, status }: FooterProductDetailProps) => {
 
   const handleChatClick = () => {
     if (product?.id) {
-      const room_id = roomIdMutation();
+      const room_id = roomIdMutation(product?.id);
       // 생성된 방으로 이동 && 실제 room_id로 수정 필요
       navigate(`/chat/room/2`);
       // navigate(`/chat/room/${room_id}`);
--- src/hooks/queries/chatting/useNewChatRoom.ts
+++ src/hooks/queries/chatting/useNewChatRoom.ts
@@ -1,9 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { instance } from 'apis';
 
-export const useNewChatRoom = (productId: number) => {
+export const useNewChatRoom = () => {
   return useMutation({
-    mutationFn: () => instance.post(`/chat/room/create?productId=${productId}&userId=1`),
+    mutationFn: (productId: number) =>
+      instance.post(`/chat/room/create?productId=${productId}&userId=1`),
     onSuccess: (res) => {
       console.log('채팅방 생성 성공', res.data);
       return res.data.chat_room_id;
