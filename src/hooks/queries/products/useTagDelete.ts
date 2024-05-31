2b12af28393443ed6c4fecfab17c1667d4821ed4
김동균 <wooheuy5752@nate.com>
2024-05-31 12:47:22 +0900

    Fix : 검색어 태그 삭제 시, 검색어 조회 쿼리 재로딩

--- src/hooks/queries/products/useTagDelete.ts
+++ src/hooks/queries/products/useTagDelete.ts
@@ -1,13 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { instance } from 'apis';
 import { userId } from 'data/shared';
 
 export const useTagDelete = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (searchName: string) =>
       instance.delete(`/products/search/delete?userId=${userId}&searchName=${searchName}`),
     onSuccess: (res) => {
       console.log('태그 삭제 성공', res);
+      queryClient.invalidateQueries({
+        queryKey: ['products', 'recent-search', userId],
+      });
     },
     onError: (error) => {
       console.log('태그 삭제에 실패했습니다.', error);
